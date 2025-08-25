229662323cbdcea6c03e94df454820d122209618
Светлана <autizm6kola@yandex.com>
2025-08-25 14:06:28 +0300

    Исправила имя JSON

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(process.env.PUBLIC_URL + '/tasks.json')
+    fetch(process.env.PUBLIC_URL + '/tasks_two.json')
       .then((res) => res.json())
       .then((data) => {
         setAllTasks(data);
