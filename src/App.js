c62ff03017cf14199e1c1dca869801a7c09c0227
Светлана <autizm6kola@yandex.com>
2025-08-25 19:15:06 +0300

    Исправила ошибки в JSON

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(process.env.PUBLIC_URL + '/tasks_two.json')
+    fetch(process.env.PUBLIC_URL + '/tasks_thri.json')
       .then((res) => res.json())
       .then((data) => {
         setAllTasks(data);
