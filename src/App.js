a3f3045d6f00fd9738bb3ea59408b75d83290e0f
Светлана <autizm6kola@yandex.com>
2025-08-31 17:40:33 +0300

    Переименование json

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(process.env.PUBLIC_URL + '/tasks_six.json')
+    fetch(process.env.PUBLIC_URL + '/tasksseven.json')
       .then((res) => res.json())
       .then((data) => {
         setAllTasks(data);
